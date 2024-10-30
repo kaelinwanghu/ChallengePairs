9c51bcdc6fdeb43ac5dbbe8b135c916b17d5dff9
kaelinwanghu <kaelin.wang.hu@gmail.com>
2024-10-30 01:00:04 -0400

    Ported over and improved on Adjacency List Graph
    Also started using hash_set8 for faster performance as well, considered stringzilla but it's going to be node_ids anyways, so no point. Might have to replace successor_list and predecessor_list with sets instead of vectors given the slow find

--- src/graph.cpp
+++ src/graph.cpp
@@ -1,1 +1,229 @@
 #include "graph.hpp"
+
+Graph::Graph() : edgeCount(0)
+{ }
+
+Graph::~Graph() 
+{ }
+
+// Gets the size of the graph in terms of vertices
+uint32_t Graph::size() const
+{
+    std::lock_guard<std::mutex> lock(graph_mutex);
+
+    return key_to_id.size();
+}
+
+// Gets the number of edges of the graph
+uint32_t Graph::num_edges() const
+{
+    std::lock_guard<std::mutex> lock(graph_mutex);
+
+    return edgeCount;
+}
+
+// Adds a vertex to the graph if it does not already exist (names should all be unique)
+bool Graph::add_vertex(const uint32_t node_id, const std::string& key)
+{
+    std::lock_guard<std::mutex> lock(graph_mutex);
+
+    // Check that they don't exist yet before adding vertex
+    if (key_to_id.find(key) != key_to_id.end() || id_to_key.find(node_id) != id_to_key.end())
+    {
+        return false;
+    }
+
+    key_to_id[key] = node_id;
+    id_to_key[node_id] = key;
+
+    // Initialize vector successors and predecessors as well
+    successor_list[node_id] = std::vector<uint32_t>();
+    predecessor_list[node_id] = std::vector<uint32_t>();
+
+    return true;
+}
+
+// Adds an edge from a node to another if they do not exist already
+bool Graph::add_edge(const uint32_t from_id, const uint32_t to_id)
+{
+    std::lock_guard<std::mutex> lock(graph_mutex);
+
+    if (!has_vertex(from_id) || !has_vertex(to_id))
+    {
+        return false;
+    }
+
+    // Rather inefficient find, change in the future (auto is left for now)
+    auto& successors = successor_list[from_id];
+    if (std::find(successors.begin(), successors.end(), to_id) != successors.end())
+    {
+        return false;
+    }
+
+    // Emplace for both successors and predecessors
+    successors.emplace_back(to_id);
+    predecessor_list[to_id].emplace_back(from_id);
+
+    ++edgeCount;
+
+    return true;
+}
+
+// Checks whether the graph has a particular vertex with the node Id
+bool Graph::has_vertex(const uint32_t node_id) const
+{
+    std::lock_guard<std::mutex> lock(graph_mutex);
+
+    return id_to_key.find(node_id) != id_to_key.end();
+}
+
+// Checks whether the graph has a particular edge with the node Id
+bool Graph::has_edge(const uint32_t from_id, const uint32_t to_id) const
+{
+    std::lock_guard<std::mutex> lock(graph_mutex);
+
+    if (!has_vertex(from_id) || !has_vertex(to_id))
+    {
+        return false;
+    }
+
+    // The graph is not extremely sparse but not extremely filled, so the change into HashSet might have to wait
+    const auto& successors = successor_list.at(from_id);
+    return std::find(successors.begin(), successors.end(), to_id) != successors.end();
+}
+
+// Removes an edge from the graph if it exists
+bool Graph::remove_edge(const uint32_t from_id, const uint32_t to_id)
+{
+    std::lock_guard<std::mutex> lock(graph_mutex);
+
+    if (!has_vertex(from_id) || !has_vertex(to_id))
+    {
+        return false;
+    }
+
+    // Remove for both successors...
+    auto& successors = successor_list[from_id];
+    auto& successor_find = std::find(successors.begin(), successors.end(), to_id);
+    if (successor_find != successors.end())
+    {
+        successors.erase(successor_find);
+    }
+    else
+    {
+        return false;
+    }
+
+    // And predecessors
+    auto& predecessors = predecessor_list[to_id];
+    auto& predecessor_find = std::find(predecessors.begin(), predecessors.end(), from_id);
+    if (predecessor_find != predecessors.end())
+    {
+        predecessors.erase(predecessor_find);
+    }
+
+    --edgeCount;
+
+    return true;
+}
+
+// Calculates the out_degree of a node (how many edges originate from it)
+uint32_t Graph::out_degree(const uint32_t node_id) const
+{
+    std::lock_guard<std::mutex> lock(graph_mutex);
+
+    auto it = successor_list.find(node_id);
+    if (it != successor_list.end())
+    {
+        return static_cast<uint32_t>(it->second.size());
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+// Calculates the out_degree of a node (how many edges end at it)
+uint32_t Graph::in_degree(const uint32_t node_id) const
+{
+    std::lock_guard<std::mutex> lock(graph_mutex);
+
+    auto it = predecessor_list.find(node_id);
+    if (it != predecessor_list.end())
+    {
+        return static_cast<uint32_t>(it->second.size());
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+// Gets the node_id based on a particular string key (person name)
+uint32_t Graph::get_node_id(const std::string& key) const
+{
+    std::lock_guard<std::mutex> lock(graph_mutex);
+
+    // Fast HashSet search
+    auto& it = key_to_id.find(key);
+    if (it != key_to_id.end())
+    {
+        return it->second;
+    }
+    else
+    {
+        return 0; // maybe throw an error instead? Not sure if there is node 0
+    }
+}
+
+// Gets the key (person name) based on the specified node_id, opposite of function above
+std::string Graph::get_key(const uint32_t node_id) const
+{
+    std::lock_guard<std::mutex> lock(graph_mutex);
+
+    auto it = id_to_key.find(node_id);
+    if (it != id_to_key.end())
+    {
+        return it->second;
+    }
+    else
+    {
+        return std::string(); // empty string return otherwise
+    }
+}
+
+// Gets the successor set of a certain node
+emhash8::HashSet<uint32_t, XXIntHasher> Graph::successor_set(const uint32_t node_id) const
+{
+    std::lock_guard<std::mutex> lock(graph_mutex);
+
+    emhash8::HashSet<uint32_t, XXIntHasher> successors;
+    auto it = successor_list.find(node_id);
+    if (it != successor_list.end())
+    {
+        for (const auto& successor_id : it->second)
+        {
+            successors.insert(successor_id);
+        }
+    }
+
+    return successors;
+}
+
+// Gets the predecessor set of a certain node
+emhash8::HashSet<uint32_t, XXIntHasher> Graph::predecessor_set(const uint32_t node_id) const
+{
+    std::lock_guard<std::mutex> lock(graph_mutex);
+
+    emhash8::HashSet<uint32_t, XXIntHasher> predecessors;
+    auto it = predecessor_list.find(node_id);
+    if (it != predecessor_list.end())
+    {
+        for (const auto& predecessor_id : it->second)
+        {
+            predecessors.insert(predecessor_id);
+        }
+    }
+
+    return predecessors;
+}
--- src/graph.hpp
+++ src/graph.hpp
@@ -1,8 +1,63 @@
-#include <hash_table8.hpp>
-#include <xxhash.h>
-#include <stdint.h>
-#include <string>
-#include <mutex>
-#include <iostream>
-#include <thread>
+#pragma once
 
+#include <hash_table8.hpp> // emhash8 hashmap
+#include <hash_set8.hpp>   // emhash8 hashset
+#include <xxhash.h>        // xxHash
+#include <stdint.h>        // For fixed-size integer types
+#include <mutex>           // For multithreading
+#include <vector>
+#include <string>
+#include <iostream>
+
+// Note that the graph edges are around 3 million, so a uint32_t should suffice
+
+// Hash functions for both ints and strings using xxHash
+struct XXStringHasher {
+    uint64_t operator()(const std::string& key) const {
+        return XXH64(key.data(), key.size(), 0);
+    }
+};
+
+struct XXIntHasher {
+    uint64_t operator()(const uint32_t& key) const {
+        return XXH64(&key, sizeof(key), 0);
+    }
+};
+
+class Graph {
+public:
+    Graph();
+    ~Graph();
+
+    uint32_t size() const;
+    uint32_t num_edges() const;
+
+    bool add_vertex(const uint32_t node_id, const std::string& key);
+    bool add_edge(const uint32_t from_id, const uint32_t to_id);
+
+    bool has_vertex(const uint32_t node_id) const;
+    bool has_edge(const uint32_t from_id, const uint32_t to_id) const;
+
+    bool remove_edge(const uint32_t from_id, const uint32_t to_id);
+
+    uint32_t out_degree(const uint32_t node_id) const;
+    uint32_t in_degree(const uint32_t node_id) const;
+
+    uint32_t get_node_id(const std::string& key) const;
+    std::string get_key(const uint32_t node_id) const;
+
+    emhash8::HashSet<uint32_t, XXIntHasher> successor_set(const uint32_t node_id) const;
+    emhash8::HashSet<uint32_t, XXIntHasher> predecessor_set(const uint32_t node_id) const;
+
+private:
+    // Might be changed in the future into a HashSet instead of a vector if it causes efficiency issues
+    emhash8::HashMap<uint32_t, std::vector<uint32_t>, XXIntHasher> successor_list;
+    emhash8::HashMap<uint32_t, std::vector<uint32_t>, XXIntHasher> predecessor_list;
+
+    emhash8::HashMap<std::string, uint32_t, XXStringHasher> key_to_id;
+    emhash8::HashMap<uint32_t, std::string, XXIntHasher> id_to_key;
+
+    uint32_t edgeCount;
+
+    mutable std::mutex graph_mutex;
+};
