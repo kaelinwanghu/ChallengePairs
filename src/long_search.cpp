e0cd8230c98bf88eaa25328c2781995afd7454ed
kaelinwanghu <kaelin.wang.hu@gmail.com>
2024-11-15 09:27:06 -0500

    Removed collapse heuristic because of its redundancy
    Just realized it would not matter anyways since the successors' successors would be blocked off, and also that it would not be an O(N) search anymore

--- src/long_search.cpp
+++ src/long_search.cpp
@@ -17,10 +17,6 @@ namespace long_search
         std::vector<std::pair<uint32_t, uint32_t>> sink_paths; // (sink_node, estimated_length)
         sink_paths.reserve(50000); // Trust me
 
-        // Tracks where the SCC has been entered
-        emhash8::HashSet<uint32_t, XXIntHasher> scc_entries;
-        scc_entries.reserve(10000);
-
         // Stores visited nodes
         emhash8::HashSet<uint32_t, XXIntHasher> visited;
         visited.reserve(graph.size());
@@ -38,35 +34,22 @@ namespace long_search
             const auto [current_estimated_length, current_node] = bfs_queue.back();
             bfs_queue.pop_back();
 
-            // If current node is a sink node (no successors), potentially long path so store it
-            if (graph.out_degree(current_node) == 0)
+            const auto& successors = graph.successors(current_node);
+
+                // If current node is a sink node (no successors), potentially long path so store it
+            if (successors.empty())
             {
                 sink_paths.emplace_back(current_node, current_estimated_length);
                 continue;
             }
 
             // For each successor
-            for (const uint32_t successor : graph.successors(current_node))
+            for (auto it = successors.rbegin(); it != successors.rend(); ++it)
             {
-                uint32_t successor_diameter = graph.get_scc_diameter(successor);
-                bool should_process = false;
-                if (graph.is_scc(current_node))
+                const uint32_t successor = *it;
+                if (visited.insert(successor).second)
                 {
-                    should_process = true;
-                }
-                // Single node
-                else if (successor_diameter == 1)
-                {
-                    should_process = visited.insert(successor).second;
-                }
-                // SCC node
-                else
-                {
-                    should_process = scc_entries.insert(successor).second;
-                }
-                if (should_process)
-                {
-                    // Emplace the successor into the priority queue
+                    uint32_t successor_diameter = graph.get_scc_diameter(successor);
                     bfs_queue.emplace_back(current_estimated_length + successor_diameter, successor);
                 }
             }
@@ -75,24 +58,14 @@ namespace long_search
         // Keep only the top TOP_PATHS sink nodes with the largest estimated lengths
         if (sink_paths.size() > TOP_PATHS)
         {
-            std::partial_sort(sink_paths.begin(),
-                sink_paths.begin() + TOP_PATHS,
+            std::nth_element(sink_paths.begin(),
+                sink_paths.begin() + TOP_PATHS - 1,
                 sink_paths.end(),
-                [](const auto& a, const auto& b)
-                {
+                [](const auto& a, const auto& b) {
                     return a.second > b.second;
                 });
-            sink_paths.resize(TOP_PATHS);
         }
-        else
-        {
-            std::sort(sink_paths.begin(), sink_paths.end(),
-            [](const auto& a, const auto& b)
-            {
-                return a.second > b.second;
-            });
-        }
-
+        
         // Evaluate the actual shortest paths for the top sink nodes
         std::pair<uint32_t, uint32_t> best_result = {0, 0}; // (path_length, sink_node)
         for (const auto& [sink_node, estimated_length] : sink_paths)
